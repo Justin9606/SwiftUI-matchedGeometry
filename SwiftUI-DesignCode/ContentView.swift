cf7a313398300379cd9026c930ee029ed2ed9e64
justin <tonybek.96@gmail.com>
2023-11-06 11:07:56 +0900

    Initial Commit

---
+++ SwiftUI-DesignCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-DesignCode
+//
+//  Created by 쟈니 on 11/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-DesignCode/SwiftUI_DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DesignCodeApp.swift
+//  SwiftUI-DesignCode
+//
+//  Created by 쟈니 on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
